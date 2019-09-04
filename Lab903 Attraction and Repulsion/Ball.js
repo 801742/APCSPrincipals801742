aa93290294f94c730c2c3a2918732b5cac8f3c6b
Home <54591166+801742@users.noreply.github.com>
2019-09-03 20:09:10 -0700

    i tried

--- Lab903 Attraction and Repulsion/Ball.js
+++ Lab903 Attraction and Repulsion/Ball.js
@@ -6,11 +6,12 @@ class Ball{
     //this.y = y;
     //this.dx = dx;
     //this.dy = dy;
-    this.clr = color(random(255), random(255), random(255))
     //this.w = random(10, 70);
+    this.clr = color(random(255), random(255), random(255))
     this.loc = createVector(x, y);
     this.vel = createVector(dx, dy);
     this.id = id;
+    this.acc = createVector(0, .1);
 }
 run(){
   this.render();
@@ -45,7 +46,27 @@ render(){
 
 
 update(){
+  if(id = -1){
+    var d = this.loc.dist(mainBall.loc);
+    if(d<450){
+      var this.Force = p5.Vector.sub(mainBall.loc, this.loc);
+      this.Force.normalize();
+      this.Force.mult(.08);
+      this.vel.add(this.Force);
+      if(d> 85){
+        var repForce = p5.Vector.sub(this.loc, mainBall.loc);
+        repForce.normalize();
+        repForce.mult(.5);
+        tnhis.vel.add(repForce);
+      }
+    }
+    else{}
+    this.vel.limit(3);
+  }
   this.loc.add(this.vel)
+  //  this.acc = p5.Vector.sub(mainBall.loc, this.loc);
+    //this.acc.normalize();
+    //this.acc.halt(0.5);
 }
 
 }
--- Lab903 Attraction and Repulsion/sketch.js
+++ Lab903 Attraction and Repulsion/sketch.js
@@ -8,13 +8,14 @@ function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(20, 20, 20);
-
+mainBall = new Ball(width, height, 1 , 1 , -1);
 loadBalls(100);
 }
 //  The draw function is called @ 30 fps
 function draw() {
   background(5,5,5);
   runBalls();
+  mainBall.run();
 }
 
 function loadBalls(n){
@@ -28,5 +29,5 @@ function runBalls(){
   for(var i = 0; i < balls.length; i++){
     balls[i].run();
   }
-  
+
 }
