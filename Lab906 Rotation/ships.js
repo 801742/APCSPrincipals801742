cc9f659b40a969dad5f9067e0e18daafaef268f9
801742 <54591166+801742@users.noreply.github.com>
2019-09-10 08:50:37 -0700

    rotation attempt

--- Lab906 Rotation/ships.js
+++ Lab906 Rotation/ships.js
@@ -1,12 +1,12 @@
 class Ship{
   constructor(x, y, dx, dy, id) {
-    this.clr = color(random(255), random(255), random(255))
+    this.clr = color(random(255), random(255), random(255));
     this.loc = createVector(x, y);
     this.vel = createVector(dx, dy);
     this.id = id;
     this.angle = 0;
     this.acc = createVector(0,0)
-  }; //end of constructor
+  } //end of constructor
 
 run(){
   this.render();
@@ -15,11 +15,12 @@ run(){
 } //end of run
 
 render(){
+  fill(this.clr);
   this.angle = this.angle +.1
   push();
-  translate(this.loc.x, this.loc.y)
-  rotate(this.angle)
-  triangle(-5, 8, 5, 8, 0, -8);
+    translate(this.loc.x, this.loc.y)
+    rotate(this.angle)
+    triangle(-5, 8, 5, 8, 0, -8);
   pop();
 } // end of render
 
@@ -27,6 +28,13 @@ checkEdges(){
   if(this.loc.x < 0) this.loc.x = width;
   if(this.loc.x > width) this.loc.x = 0;
   if(this.loc.y < 0) this.loc.y = height;
+  if(this.loc.y > height) this.loc.y = 0
+}
+
+update(){
+  this.vel.add(ths.acc);
+  this.vel.limit(5)
+  this.loc.add(this.vel);
 }
 
 }//end of ship class
--- Lab906 Rotation/sketch.js
+++ Lab906 Rotation/sketch.js
@@ -2,14 +2,14 @@
 // 	9/6/19
 // Rotation
 
-var ships = []
-var mainBall =[]
-var mainBallrep = []
+var ships = [];
+var mainBall =[];
+var mainBallrep = [];
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(20, 20, 20,);
-loadBalls(800);
+loadShips(100);
 }
 //  The draw function is called @ 30 fps
 function draw() {
@@ -18,7 +18,7 @@ function draw() {
 
 }
 
-function loadBalls(n){
+function loadShips(n){
   mainBallrep = new Ball(width/2, height/2, random(-1, 1), random(-1,1), -2);
   mainBall = new Ball(width/2, height/2, random(-1, 1), random(-1,1), -1);
     for(var i =0; i < n; i++){
