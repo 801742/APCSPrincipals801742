a47c5ed0908bb16737fd1be5d334ccb11c896aa9
801742 <54591166+801742@users.noreply.github.com>
2019-10-09 08:50:12 -0700

    hard

--- Lab1007 Checkerboard/rectangle.js
+++ Lab1007 Checkerboard/rectangle.js
@@ -1,14 +1,13 @@
 class Rectangle{
-  constructor(x, y, w, h){
+  constructor(x, y, clr){
     this.x = x;
     this.y = y;
-    this.w = w;
     this.h = h;
-    this.clr = color(random(255), random(255), random(255));
+    this.clr = color(clr, clr, clr)
   }
   render(){
     fill(this.clr);
-    rect(this.x, this.y, this.w, this.h);
+    rect(this.x, this.y, 100, 100);
   } //end of render
   run(){
     this.render();
--- Lab1007 Checkerboard/sketch.js
+++ Lab1007 Checkerboard/sketch.js
@@ -2,12 +2,14 @@
 // 	Date or version number
 //  This is a comment
 //  The setup function function is called once when your program begins
+var clr;
+var h;
 var squares = [];
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(5, 5, 5);
-  loadRect()
+  loadRect();
 }
 
 //  The draw function is called @ 30 fps
@@ -15,13 +17,17 @@ function draw() {}
 
 function loadRect(){
   idy = 0
+  c = 0
 for(i = 0; i < 8; i++){
   p = 50
   id = 0
   idy = idy + 1
 for(j = 0; j < 8; j++){
+  c = c + 1
+  if(c & 2 === 0) clr = 255
+  if(c & 2 === 1) clr = 1
   id = id+1
-  squares = new Rectangle(p*id, p*idy, 50, 50);
+  squares = new Rectangle(p*id, p*idy, clr);
 }
 }
 }
