d7c5c40db5ebd366af01070cb307bc95f860237c
801742 <54591166+801742@users.noreply.github.com>
2019-10-11 08:50:08 -0700

    E

---
+++ Lab1009 BubbleSort/sketch.js
@@ -0,0 +1,18 @@
+//  Andrew Mills
+// 	10/09
+// Bubble Sort
+
+function setup() {
+  var cnv = createCanvas(800, 800);
+  cnv.position((windowWidth-width)/2, 30);
+  background(5, 5, 5);
+  fill(200, 30, 150);
+
+
+}
+
+//  The draw function is called @ 30 fps
+function draw() {
+
+
+}
