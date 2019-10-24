97c35c30fd5c5faf79d96a27141006fbe541a251
801742 <54591166+801742@users.noreply.github.com>
2019-10-24 08:50:03 -0700

    dv

---
+++ Lab1023 BarSort/bars.js
@@ -0,0 +1,13 @@
+class Bars{
+  constructor(location){
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.clr = color(24, 150, 70);
+  };
+  render(){
+    fill(this.clr)
+    rect(this.x, this.y, this.w, this.h);
+  }
+}
--- Lab1023 BarSort/sketch.js
+++ Lab1023 BarSort/sketch.js
@@ -2,18 +2,33 @@
 // 	Date or version number
 //  This is a comment
 //  The setup function function is called once when your program begins
-var list = [];
+var bars = [];
+var numBars, barWidth;
+
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(5, 5, 5);
   fill(200, 30, 150);
-  for (var i = 0; i < 10; i++) list[i] = int(random(10));
-  selectionSwap();
+
+  barWidth = 10;
+  numBars = width/barWidth;
+
+  loadBars(numBars);
+  runBars();
+  myBubbleSort();
 }
 
 //  The draw function is called @ 30 fps
-function draw() {}
+function draw() {
+  background(20, 20, 110);
+  frameRate(1);
+  runBars();
+}
+
+function loadBars(num){
+    
+}
 function selectionSwap(){
 for (var i = 0; i < list.length - 1; i++){
   var index = i;
@@ -28,18 +43,3 @@ for (var i = 0; i < list.length - 1; i++){
         }
       }
 console.log(list)
-
-
-class Bars{
-  constructor(x, y, w, h){
-    this.x = x;
-    this.y = y;
-    this.w = w;
-    this.h = h;
-    this.clr = color(24, 150, 70);
-  };
-  render(){
-    fill(this.clr)
-    rect(this.x, this.y, this.w, this.h)
-  }
-}
