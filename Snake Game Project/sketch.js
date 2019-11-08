fcb03c2379c4164399585dfeffd46134d15628fe
801742 <54591166+801742@users.noreply.github.com>
2019-11-08 09:49:51 -0800

    com

--- Snake Game Project/sketch.js
+++ Snake Game Project/sketch.js
@@ -4,6 +4,7 @@
 //  The setup function function is called once when your program begins
 var head;
 var body = []
+var grid = []
 function setup(){
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
@@ -12,10 +13,21 @@ function setup(){
   loadHead();
   runHead();
 }
-
+function draw(){
+  grid();
+}
 function loadHead(){
   head = new Snake(random(800), random(800), 25, 25)
 }
 function runHead(){
   head.run();
+}
+function grid(){
+  for(i = 0; i < 8; i++){
+    for(n = 0; n < 8; n++){
+      if(n % 2 = 1){ fill(255, 0, 0) }
+      if(n % 2 = 0){ fill(0, 255, 0) }
+      rect((width/8)*n,(height/8)*i,200, 200)
+    }
+  }
 }
