378264d84492f301bdbba526a94bfe9fb527cd73
801742 <54591166+801742@users.noreply.github.com>
2019-11-12 09:50:23 -0800

    comk

--- Snake Game Project/sketch.js
+++ Snake Game Project/sketch.js
@@ -3,8 +3,6 @@
 //  This is a comment
 //  The setup function function is called once when your program begins
 var head;
-var body = []
-var grid = []
 function setup(){
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
@@ -12,22 +10,12 @@ function setup(){
   fill(200, 30, 150);
   loadHead();
   runHead();
-}
-function draw(){
-  grid();
+  frameRate(15);
 }
 function loadHead(){
-  head = new Snake(random(800), random(800), 25, 25)
+  head = new Snake(random(800), random(800), 25, 0, 0)
 }
 function runHead(){
+  background(20, 20, 20);
   head.run();
-}
-function grid(){
-  for(i = 0; i < 8; i++){
-    for(n = 0; n < 8; n++){
-      if(n % 2 = 1){ fill(255, 0, 0) }
-      if(n % 2 = 0){ fill(0, 255, 0) }
-      rect((width/8)*n,(height/8)*i,200, 200)
-    }
-  }
 }
--- Snake Game Project/snake.js
+++ Snake Game Project/snake.js
@@ -1,36 +1,37 @@
 
 class Snake{
-  constructor(x, y, w){
-    this.loc = createVector(x,y)
+  constructor(x, y, w, dx, dy){
+    this.loc = createVector(x,y);
     this.w = w;
     this.clr = color(255, 0, 0);
-    this.vel = createVector(0,0)
-  }
+    this.vel = createVector(dx, dy);
+  };
   render(){
-    fill(this.clr)
-    rect(this.x, this.y, this.w, this.w)
-  }
+    fill(this.clr);
+    rect(this.loc.x, this.loc.y, this.w, this.w);
+  };
   run(){
     this.render();
-  }
+    this.update();
+  };
   update(){
-    if(KeyIsPressed &&
+    if(keyIsPressed &&
       keyCode === RIGHT_ARROW){
-        this.vel.x = 3
-        this.vel.y = 0
-      }else if(KeyIsPressed &&
+        this.vel.x = 1;
+        this.vel.y = 0;
+      }else if(keyIsPressed &&
       keyCode === LEFT_ARROW){
-        this.vel.x = -3
-        this.vel.y = 0
-      }else if(KeyIsPressed &&
-          keyCode === DOWN_ARROW){
-            this.vel.x = 0
-            this.vel.y = -3
-          }else if(KeyIsPressed &&
-          keyCode === UP_ARROW){
-            this.vel.x = 0
-            this.vel.y = 3
-          }
-    this.loc.add(this.vel);
-  }
-}
+        this.vel.x = -1;
+        this.vel.y = 0;
+      }else if(keyIsPressed &&
+      keyCode === DOWN_ARROW){
+        this.vel.y = -1;
+        this.vel.x = 0;
+      }else if(keyIsPressed &&
+      keyCode === UP_ARROW){
+        this.vel.y = 1;
+        this.vel.x = 0;
+    }
+      this.loc.add(this.vel);
+  };
+};
